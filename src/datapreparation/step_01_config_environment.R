eed7973fb12c10f8a27938b70d2bfe5ddfd534aa
RodriGonca <rodrigo.goncalves@me.com>
2020-01-03 12:23:21 -0300

    small step to check for required packages not installed.

--- src/datapreparation/step_01_config_environment.R
+++ src/datapreparation/step_01_config_environment.R
@@ -13,3 +13,20 @@ directoryPath  <- stringr::str_replace(directoryPath, "/src/datapreparation", ""
 
 setwd(directoryPath)
 getwd()
+
+# check required libraries
+
+list.of.packages <- c("rawr", 'reticulate', 'ggwordcloud')
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if (length(new.packages)) {
+  print('one or more required package for this project is not installed in your environment.')
+  print(new.packages)
+  print('would you like to install it?')
+  user.reply <- readline(prompt="y/n: ")
+  if (user.reply == 'y') {
+    install.packages(new.packages)
+  }
+}
+
