4f1b77d2951494716d1df2c1b3cbc684593b6405
ldaniel <contato@leandrodaniel.com>
2020-01-07 11:38:29 -0300

    Refactor file names in dataprep directory to better accommodate the tasks

--- src/datapreparation/execute_data_preparation.R
+++ src/datapreparation/execute_data_preparation.R
@@ -12,7 +12,8 @@ source("./src/util/sync_kaggle.R")
 
 # executing data preparation steps --------------------------------------------
 source("./src/datapreparation/step_01_config_environment.R")
-source("./src/datapreparation/step_02_data_ingestion.R")
-source("./src/datapreparation/step_03_data_cleaning.R")
-source("./src/datapreparation/step_04_data_enhancement.R")
-source("./src/datapreparation/step_05_dataset_preparation.R")
+source("./src/datapreparation/step_02_data_download.R")
+source("./src/datapreparation/step_03_data_ingestion.R")
+source("./src/datapreparation/step_04_data_cleaning.R")
+source("./src/datapreparation/step_05_data_enhancement.R")
+source("./src/datapreparation/step_06_dataset_preparation.R")
