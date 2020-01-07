36f2dab1ce57e2b650de6700e5fc7a01e11f5886
ldaniel <contato@leandrodaniel.com>
2020-01-07 14:46:55 -0300

    Update date enhancement script to log its execution

--- src/datapreparation/step_05_data_enhancement.R
+++ src/datapreparation/step_05_data_enhancement.R
@@ -2,4 +2,19 @@
 
 # enhancement of data ---------------------------------------------------------
 
-#to-do
+start_time = Sys.time()
+
+lyrics$decade <- paste(str_sub(lyrics$year, 1, 3), '0', sep = '')
+lyrics$genre  <- trimws(lyrics$genre)
+
+total_lyrics_loaded <- length(lyrics$index)
+end_time = Sys.time()
+
+log  <- paste('[', Sys.time(), '] ',
+              'Task: step_05_data_enhancement.R | ', 
+              'Total time : ', end_time - start_time, ' | ',
+              'Total records: ', total_lyrics_loaded, ' | ',
+              'Memory used: ', pryr::mem_used(), ' bytes',
+              sep = "")
+
+write(log, file = "log.txt", append = TRUE)
