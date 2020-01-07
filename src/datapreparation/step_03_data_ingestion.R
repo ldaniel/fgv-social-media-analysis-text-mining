4c7f4803f0f0848372835e94bcab67b299d6ed86
ldaniel <contato@leandrodaniel.com>
2020-01-07 14:19:32 -0300

    Update gitignore to prevent log files to be included

--- src/datapreparation/step_03_data_ingestion.R
+++ src/datapreparation/step_03_data_ingestion.R
@@ -4,7 +4,6 @@
 start = Sys.time()
 
 dataDirectory <- "./data/raw/"
-
 lyrics <- read_csv(paste(dataDirectory, "lyrics.csv", sep = ""),
                    col_types = cols(
                      index = col_integer(),
@@ -15,11 +14,14 @@ lyrics <- read_csv(paste(dataDirectory, "lyrics.csv", sep = ""),
                      lyrics = col_character()),
                    locale = locale(encoding = 'UTF-8'))
 
-print(paste('Número de observações: ', length(lyrics$index)))
+total_lyrics_loaded <- length(lyrics$index)
+end = Sys.time()
 
-end   = Sys.time()
-end - start
-pryr::mem_used()
+log  <- paste('[', Sys.time(), '] ',
+              'Task: step_03_data_ingestion.R | ', 
+              'Total time : ', end - start, ' | ',
+              'Total records: ', total_lyrics_loaded, ' | ',
+              'Memory used: ', pryr::mem_used(), ' bytes',
+              sep = "")
 
-log  <- paste(end - start, ' - ', 'import lyrics', ' - memory: ', pryr::mem_used())
 write(log, file = "log.txt", append = TRUE)
