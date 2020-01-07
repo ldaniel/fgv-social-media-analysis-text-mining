2603999f3904036bae04e935bb9873d900432949
ldaniel <contato@leandrodaniel.com>
2020-01-07 14:53:20 -0300

    Update date preparation script to log its execution

--- src/datapreparation/step_06_dataset_preparation.R
+++ src/datapreparation/step_06_dataset_preparation.R
@@ -5,5 +5,17 @@
 # The objective of this step is to return a DataFrame to be used in the
 # predictive modeling.
 
+start_time = Sys.time()
+
 # saving the final dataset to evaluate
-saveRDS(target_data, '../data/processed/target_dataset.rds')
+saveRDS(lyrics, '../data/processed/lyrics.rds')
+
+end_time = Sys.time()
+
+log  <- paste('[', Sys.time(), '] ',
+              'Task: step_06_data_preparation.R | ', 
+              'Total time : ', end_time - start_time, ' | ',
+              'Memory used: ', pryr::mem_used(), ' bytes',
+              sep = "")
+
+write(log, file = "log.txt", append = TRUE)
