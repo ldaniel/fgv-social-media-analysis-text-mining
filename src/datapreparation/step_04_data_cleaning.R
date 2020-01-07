023e6e82cbfd3db5b5188dd0e005452665980ed4
ldaniel <contato@leandrodaniel.com>
2020-01-07 14:44:16 -0300

    Update date clearing script to log its execution

--- src/datapreparation/step_04_data_cleaning.R
+++ src/datapreparation/step_04_data_cleaning.R
@@ -2,4 +2,25 @@
 
 # analysing missing values and other strange conditions -----------------------
 
-#to-do
+start_time = Sys.time()
+
+lyrics <- filter(lyrics, 
+                 !is.na(lyrics), 
+                 !(genre %in% c('Not Available', 'Other')),
+                 as.integer(year) >= 1970)
+
+# uncomment for fast prototyping
+# lyrics  <- sample_n(lyrics, size = 150000)
+# invisible(gc())
+
+total_lyrics_loaded <- length(lyrics$index)
+end_time = Sys.time()
+
+log  <- paste('[', Sys.time(), '] ',
+              'Task: step_04_data_clearing.R | ', 
+              'Total time : ', end_time - start_time, ' | ',
+              'Total records: ', total_lyrics_loaded, ' | ',
+              'Memory used: ', pryr::mem_used(), ' bytes',
+              sep = "")
+
+write(log, file = "log.txt", append = TRUE)
