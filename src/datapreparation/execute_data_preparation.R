e6136914aa1e82c4b240666d928e428cb48cceff
ldaniel <contato@leandrodaniel.com>
2020-01-07 17:37:41 -0300

    Comment not finished scripts

--- src/datapreparation/execute_data_preparation.R
+++ src/datapreparation/execute_data_preparation.R
@@ -1,18 +1,19 @@
 # loading required libraries --------------------------------------------------
 
 # libraries for data prep
-library(readxl)
+library(readr)
 library(dplyr)
+library(stringr)
 
 # load auxiliary functions ----------------------------------------------------
 source("./src/util/auxiliary_functions.R")
 
 # sync kaggle kernel ----------------------------------------------------------
-source("./src/util/sync_kaggle.R")
+#source("./src/util/sync_kaggle.R")
 
 # executing data preparation steps --------------------------------------------
 source("./src/datapreparation/step_01_config_environment.R")
-source("./src/datapreparation/step_02_data_download.R")
+#source("./src/datapreparation/step_02_data_download.R")
 source("./src/datapreparation/step_03_data_ingestion.R")
 source("./src/datapreparation/step_04_data_cleaning.R")
 source("./src/datapreparation/step_05_data_enhancement.R")
