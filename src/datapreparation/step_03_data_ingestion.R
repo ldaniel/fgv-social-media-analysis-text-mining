d20d218bea3d6d4607254ba3bee443c923f731b5
ldaniel <contato@leandrodaniel.com>
2020-01-07 13:58:00 -0300

    Update gitignore to prevent log files to be included

--- src/datapreparation/step_03_data_ingestion.R
+++ src/datapreparation/step_03_data_ingestion.R
@@ -1,9 +1,25 @@
 ## ---- step_03_data_ingestion.R
 
 # performing data loading -----------------------------------------------------
-dataDirectory <- "../data/raw/"
+start = Sys.time()
 
-target_data <- read_xlsx(paste(dataDirectory, "base_casamentos.xlsx", sep = ""), 
-                         sheet = "CASAMENTOS", 
-                         skip = 1, 
-                         col_names = TRUE)
+dataDirectory <- "./data/raw/"
+
+lyrics <- read_csv(paste(dataDirectory, "lyrics.csv", sep = ""),
+                   col_types = cols(
+                     index = col_integer(),
+                     song = col_character(),
+                     year = col_integer(),
+                     artist = col_factor(),
+                     genre = col_factor(),
+                     lyrics = col_character()),
+                   locale = locale(encoding = 'UTF-8'))
+
+print(paste('Número de observações: ', length(lyrics$index)))
+
+end   = Sys.time()
+end - start
+pryr::mem_used()
+
+log  <- paste(end - start, ' - ', 'import lyrics', ' - memory: ', pryr::mem_used())
+write(log, file = "log.txt", append = TRUE)
