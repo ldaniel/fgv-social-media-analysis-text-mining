53a5284d0f36ff8f7e414c1f34746ebe2be73c58
ldaniel <contato@leandrodaniel.com>
2020-01-07 17:02:16 -0300

    Update with first download mechanism option

--- src/datapreparation/step_02_data_download.R
+++ src/datapreparation/step_02_data_download.R
@@ -1,3 +1,19 @@
 ## ---- step_02_data_download.R
 
-# performing data dowloading --------------------------------------------------
+# performing data download from Kaggle ----------------------------------------
+
+# for this project, we are using the following Kaggle dataset:
+# -> https://www.kaggle.com/gyani95/380000-lyrics-from-metrolyrics
+#
+# to make it work you will need to install kaggle public API:
+#   -> https://github.com/Kaggle/kaggle-api#Installation
+#
+# and set up your API credentials.
+#   -> https://github.com/Kaggle/kaggle-api#api-credentials
+#
+# make sure to save your API credentials at: C:\Users\<username>\.kaggle\kaggle.json
+#
+# following, the API command to download the dataset from Kaggle:
+#     kaggle datasets download -d gyani95/380000-lyrics-from-metrolyrics
+
+# to-do
