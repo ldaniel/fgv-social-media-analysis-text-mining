d4e07e1a27207875541a39681e5788305acc30d5
ldaniel <contato@leandrodaniel.com>
2020-01-08 11:15:13 -0300

    Add tokenization dataprep

--- src/datapreparation/step_03_data_ingestion.R
+++ src/datapreparation/step_03_data_ingestion.R
@@ -6,6 +6,7 @@
 #   read_csv is from the readr package;
 #   read_csv is faster than read.csv for big files. 
 
+# loading lyrics ----
 start_time = Sys.time()
 
 dataDirectory <- paste(directoryPath, "/data/raw/", sep = "")
@@ -26,3 +27,21 @@ end_time = Sys.time()
 WriteLog(TaskName = "step_03_data_ingestion.R", 
          TotalTime = end_time - start_time, 
          AdditionalInfo = total_lyrics_loaded)
+
+# applying tokenization in the lirycs ----
+start_time = Sys.time()
+
+lyrics_token <- unnest_tokens(lyrics,
+                              input = lyrics,
+                              output = word,
+                              token = 'words',
+                              drop = TRUE,
+                              to_lower = TRUE)
+
+total_words <- paste('Total words after tokenization: ', length(lyrics_token$index), sep = "")
+end_time = Sys.time()
+
+# call logging function
+WriteLog(TaskName = "step_03_data_ingestion.R", 
+         TotalTime = end_time - start_time, 
+         AdditionalInfo = total_words)
