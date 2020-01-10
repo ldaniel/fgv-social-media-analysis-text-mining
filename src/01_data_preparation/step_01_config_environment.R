439083b32be52db17436ac0d56ed2c1de0e01915
ldaniel <contato@leandrodaniel.com>
2020-01-09 21:02:01 -0300

    Fix directory path

--- src/01_data_preparation/step_01_config_environment.R
+++ src/01_data_preparation/step_01_config_environment.R
@@ -11,7 +11,7 @@ info.sysname   <- Sys.info()[["sysname"]]
 info.machine   <- Sys.info()[["machine"]]
 info.encoding  <- getOption("encoding")
 directoryPath  <- dirname(rstudioapi::getSourceEditorContext()$path)
-directoryPath  <- stringr::str_replace(directoryPath, "/src/datapreparation", "")
+directoryPath  <- stringr::str_replace(directoryPath, "/src/01_data_preparation", "")
 
 setwd(directoryPath)
 getwd()
